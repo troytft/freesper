0f036088381f70331e93c95356c5f83e9ff34aa2
Ilya Gordienko <troytft@gmail.com>
2026-06-02 01:53:05 +0400

    fix: updater ux fix (#17)

--- Sources/Freesper/AppGraph.swift
+++ Sources/Freesper/AppGraph.swift
@@ -108,6 +108,9 @@ final class AppGraph {
     }
 
     bootstrap()
+    onboardingCoordinator.onComplete = { [weak self] in
+      self?.updater.start()
+    }
     onboardingCoordinator.start()
   }
 
