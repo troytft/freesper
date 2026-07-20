55af91e60ad7586c49886b48631ddd3778df2d16
Ilya Gordienko <troytft@gmail.com>
2026-07-20 18:02:06 +0400

    add sentry

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -14,5 +14,6 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/FluidInference/FluidAudio", exact: "0.14.4"),
         .package(url: "https://github.com/sparkle-project/Sparkle", exact: "2.9.2"),
+        .package(url: "https://github.com/getsentry/sentry-cocoa", exact: "9.22.0"),
     ]
 )
